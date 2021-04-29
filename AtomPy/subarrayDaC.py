6dcc44cefe840eace3279291718d8dfdc8304cc9
Cerlanda <sach16@student.aau.dk>
2021-04-29 15:25:38 +0200

    Update subarrayDaC.py

--- AtomPy/subarrayDaC.py
+++ AtomPy/subarrayDaC.py
@@ -1,32 +1,35 @@
 #defining
-def maxCrossArraySum(A, low, mid, high):
+def maxCrossSum(A, low, mid, high):
     sum = 0
-    left_sum = -999
+    left_sum = -999 #supposed to be minus infinity like in the book
     for i in range(mid, low-1, -1):
         sum = sum + A[i]
-        if (sum > left_sum):
+        if (sum > left_sum): #update according to statement
             left_sum = sum
-    sum = 0
-    right_sum = -999
+    sum = 0 #reuse integer to less memory usage?
+    right_sum = -999 #supposed to be minus infinity like in the book
     for j in range(mid + 1, high +1):
         sum = sum + A[j]
-        if (sum > right_sum):
+        if (sum > right_sum): #update according to statement
             right_sum = sum
 
-    return max(left_sum, right_sum, left_sum + right_sum)
+    return max(left_sum, right_sum, left_sum + right_sum) #return values
 
-def maxSubArraySum(A, low, high):
+def maxSubSum(A, low, high):
     if (low == high):
         return A[low]
 
     mid = (low + high) // 2
 
-    return max(maxSubArraySum(A, low, mid),
-               maxSubArraySum(A, mid+1, high),
-               maxCrossArraySum(A, low, mid, high))
+    #A[low to mid]
+    #A[mid+1 to high]
+    #max of left sum, right sum and crossing
+    return max(maxSubSum(A, low, mid),
+               maxSubSum(A, mid+1, high),
+               maxCrossSum(A, low, mid, high))
 
-A = [-2, 3, -4, 5, 7, -8, 12, -3]
-n = len(A)
+A = [-2, 3, -4, 5, 7, -8, 12, -3] #array to be used, can be changed
+n = len(A) #len = length
 
-max_sum = maxSubArraySum(A, 0, n-1)
-print("Maximum contiguous sum is ", max_sum)
+max_sum = maxSubSum(A, 0, n-1)
+print("Maximum contiguous sum is ", max_sum) #print max contiguous sum
